b189d79adc3ea72dab24f24d40e2264cee414bd3
Antoine Neveux <antoine.neveux@gmail.com>
2013-06-28 00:39:04 +0200

    Added first POJO for data. No javadoc yet, will come later.
    Signed-off-by: Antoine Neveux <antoine.neveux@gmail.com>

---
+++ yanol-core/src/main/java/com/github/jtooling/yanol/api/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package will contain only interfaces, abstract classes, and enum if needed. 
+ * It allows to define all the interfaces you could use in order to interact with YANoL.
+ * 
+ * @author aneveux
+ */
+package com.github.jtooling.yanol.api;
---
+++ yanol-core/src/main/java/com/github/jtooling/yanol/data/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package will contain all the objects to be used as data containers in YANoL. It should mostly consist in POJOs.
+ * 
+ * @author aneveux
+ */
+package com.github.jtooling.yanol.data;
